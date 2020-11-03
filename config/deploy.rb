e80559f9fd352293beb386f15a063bcd32047991
joao <jpoliveira11@gmail.com>
2020-11-03 10:49:40 +0000

    update ruby
    remove yarn as dependency
    
    Revert "remove yarn as dependency"
    
    This reverts commit 772d3722fc7b8dc683637c758cc81b13f8f088a4.
    
    add yarn installation
    
    mtry different installs for yarn
    
    add webpacker

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,8 +4,7 @@ lock "~> 3.14.1"
 set :application, "ansible_tutorial"
 set :repo_url, "git://github.com/jpboliv/ansible_tutorial.git"
 set :deploy_to, "/home/deploy/apps"
-set :chruby_ruby, 'ruby-2.5.0'
+set :chruby_ruby, 'ruby-2.6.5'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system'
 
 require_relative 'deploy/plugins/capistrano_rails'
-require_relative 'deploy/plugins/capistrano_bundler'
